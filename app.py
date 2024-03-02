cd281e331f649e88714bb8caab049fc53142f863
bistaprapannabista <bistaprapannabista@gmail.com>
2024-03-03 00:15:28 +0545

    changes

--- app.py
+++ app.py
@@ -92,4 +92,5 @@ def home():
     return "Homepage"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -57,17 +57,17 @@ function App() {
     <>
       <h1>Laptop Price Prediction</h1>
       <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '1rem', width: "80vw" }}>
-        <div className='input-container'><label htmlFor='weight'>Weight:</label><CreatableSelect onChange={(e: { value: string, label: string }) => selectHandler(e, 'weight')} name="weight" placeholder="Enter Weight" /></div>
-        <div className='input-container'><label htmlFor='screen_size'>Screen Size:</label><CreatableSelect onChange={(e: { value: string, label: string }) => selectHandler(e, 'screen_size')} name="screen_size" placeholder="Enter Screen Size" /></div>
+        <div className='input-container'><label htmlFor='weight'>Weight:</label><CreatableSelect onChange={(e: { value: string, label: string }) => selectHandler(e, 'weight')} name="weight" placeholder="Enter Weight (KG)" /></div>
+        <div className='input-container'><label htmlFor='screen_size'>Screen Size:</label><CreatableSelect onChange={(e: { value: string, label: string }) => selectHandler(e, 'screen_size')} name="screen_size" placeholder="Enter Screen Size (In)" /></div>
         <div className='input-container'><label htmlFor='company'>Company:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'company')} name="company" placeholder="Select Company" options={options?.company} /></div>
         <div className='input-container'><label htmlFor='type'>Type:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'type')} name="type" placeholder="Select Type Name" options={options?.type} /></div>
-        <div className='input-container'><label htmlFor='ram'>Ram:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'ram')} name="ram" placeholder="Select Ram" options={options?.ram} /></div>
+        <div className='input-container'><label htmlFor='ram'>Ram:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'ram')} name="ram" placeholder="Select Ram (GB)" options={options?.ram} /></div>
         <div className='input-container'><label htmlFor='touchscreen'>Touch Screen:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'touchscreen')} name="touchscreen" placeholder="Select Touch Screen" options={options?.tocuhscreen} /></div>
         <div className='input-container'><label htmlFor='ips'>IPS:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'ips')} name="ips" placeholder="Select IPS" options={options?.ips} /></div>
         <div className='input-container'><label htmlFor='resolution'>Screen Size:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'resolution')} name="resolution" placeholder="Select Resolution" options={options?.resolution} /></div>
         <div className='input-container'><label htmlFor='cpu'>CPU:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'cpu')} name="cpu" placeholder="Select Cpu Brand" options={options?.cpu} /></div>
-        <div className='input-container'><label htmlFor='hdd'>HDD:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'hdd')} name="hdd" placeholder="Select HDD" options={options?.hdd} /></div>
-        <div className='input-container'><label htmlFor='ssd'>SSD:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'ssd')} name="ssd" placeholder="Select SSD" options={options?.sdd} /></div>
+        <div className='input-container'><label htmlFor='hdd'>HDD:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'hdd')} name="hdd" placeholder="Select HDD (GB)" options={options?.hdd} /></div>
+        <div className='input-container'><label htmlFor='ssd'>SSD:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'ssd')} name="ssd" placeholder="Select SSD (GB)" options={options?.sdd} /></div>
         <div className='input-container'><label htmlFor='gpu'>GPU:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'gpu')} name="gpu" placeholder="Select Gpu Brand" options={options?.gpu} /></div>
         <div className='input-container'><label htmlFor='os'>OS:</label><Select onChange={(e: { value: string, label: string }) => selectHandler(e, 'os')} name="os" placeholder="Select OS" options={options?.os} /></div>
       </div>
