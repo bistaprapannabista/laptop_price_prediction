c9f6e381d11d8f6fef705754f0e53a0e07a5e7a2
anjala03 <bhattaanjala13@gmail.com>
2024-12-29 20:45:29 +0545

    first commit

--- frontend/src/components/Card.tsx
+++
@@ -1,7 +0,0 @@
-const Card = (props: { children: JSX.Element }) => {
-    return (
-        <div style={{ background: "white", borderRadius: "10px", width: "100%", margin: "10px", padding: "10px" }}>{props.children}</div>
-    )
-}
-
-export default Card
